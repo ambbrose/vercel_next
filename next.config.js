efdae5eb592c4986c9b3e13b059df4472241ee8c
CodeVeegan <78057753+ambbrose@users.noreply.github.com>
2023-05-13 15:21:27 +0100

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -3,6 +3,16 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'githubusercontent.com',
+        port: '',
+        pathname: '/ambbrose/nextjs/main/images/**',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
---
+++ types.d.tsx
@@ -0,0 +1,66 @@
+type User = {
+  "id": number;
+  "name": string;
+  "username": string;
+  "email": string;
+  "address": {
+    "street": string;
+    "suite": string;
+    "city": string;
+    "zipcode": string;
+    "geo": {
+      "lat": string;
+      "lng": string;
+    };
+  };
+  "phone": string;
+  "website": string;
+  "company": {
+    "name": string;
+    "catchPhrase": string;
+    "bs": string;
+  };
+};
+
+type Post = {
+  "userId": number,
+  "id": number,
+  "title": string,
+  "body": string
+}
+
+type Result = {
+  pageid: string,
+  title: string,
+  extract: string,
+  thumbnail?: {
+    source: string,
+    width: number,
+    height: number
+  }
+}
+
+type SearchResult = {
+  query?: {
+    pages?: Result[]
+  }
+}
+
+type Meta = {
+  id: string,
+  title: string,
+  date: string,
+  tags: string[]
+}
+
+type BlogPost = {
+  meta: Meta,
+  content: any
+}
+
+type Todo = {
+  userId: number,
+  id: number,
+  title: string,
+  completed: boolean
+}
